6d2171bb5c5be2dd29428bcf1e49ab1ac922a564
Euan Magurran <Euan Magurran>
2016-05-20 00:31:43 +0100

    Have implemented HMR

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+
+const webpack = require('webpack');
+
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build')
+};
+
+
+const common = {
+	entry: {
+	app: PATHS.app
+	},
+	output: {
+	path: PATHS.build,
+	filename: 'bundle.js'
+	}
+};
+
+if(TARGET === 'start' || !TARGET){
+	module.exports = merge(common, {
+		devServer: {
+			contentBase: PATHS.build,
+			
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+			
+			stats: 'errors-only',
+			host: process.env.HOST,
+			port: process.env.PORT
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin()
+		]
+	});
+}
+if(TARGET === 'build'){
+	module.exports = merge(common, {});
+}
