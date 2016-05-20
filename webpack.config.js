eb5dc4711bbd15e5dc08284252d6aeb375f3ac5f
Euan Magurran <Euan Magurran>
2016-05-20 10:06:43 +0100

    Have added css

--- webpack.config.js
+++ webpack.config.js
@@ -7,40 +7,48 @@ const TARGET = process.env.npm_lifecycle_event;
 const webpack = require('webpack');
 
 const PATHS = {
-	app: path.join(__dirname, 'app'),
-	build: path.join(__dirname, 'build')
+	app : path.join(__dirname, 'app'),
+	build : path.join(__dirname, 'build')
 };
-
 
 const common = {
-	entry: {
-	app: PATHS.app
+	entry : {
+		app : PATHS.app
 	},
-	output: {
-	path: PATHS.build,
-	filename: 'bundle.js'
+	output : {
+		path : PATHS.build,
+		filename : 'bundle.js'
+	},
+	module: {
+		loaders: [
+		{
+			test: /\.css$/,
+			loaders: ['style', 'css'],
+			include: PATHS.app
 	}
-};
+	]
+	}
+	};
 
-if(TARGET === 'start' || !TARGET){
-	module.exports = merge(common, {
-		devServer: {
-			contentBase: PATHS.build,
-			
-			historyApiFallback: true,
-			hot: true,
-			inline: true,
-			progress: true,
-			
-			stats: 'errors-only',
-			host: process.env.HOST,
-			port: process.env.PORT
-		},
-		plugins: [
-			new webpack.HotModuleReplacementPlugin()
-		]
-	});
-}
-if(TARGET === 'build'){
-	module.exports = merge(common, {});
-}
+	if (TARGET === 'start' || !TARGET) {
+		module.exports = merge(common, {
+				devServer : {
+					contentBase : PATHS.build,
+
+					historyApiFallback : true,
+					hot : true,
+					inline : true,
+					progress : true,
+
+					stats : 'errors-only',
+					host : process.env.HOST,
+					port : process.env.PORT
+				},
+				plugins : [
+					new webpack.HotModuleReplacementPlugin()
+				]
+			});
+	}
+	if (TARGET === 'build') {
+		module.exports = merge(common, {});
+	}
