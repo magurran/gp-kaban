c8d92bc3b5a169b637bedbfe369d91dc3c275761
Euan Magurran <Euan Magurran>
2016-05-24 09:55:22 +0100

    Implemented local storage to maintain data when web page is refreshed

--- app/index.jsx
+++ app/index.jsx
@@ -2,10 +2,13 @@ import './main.css';
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
+persist(alt, storage, 'app');
 
 ReactDom.render(<App/>, document.getElementById('app'));
-
-//document.body.appendChild(component());
 
 module.exports ={
 	plugins:[
